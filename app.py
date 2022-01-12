e16434c71016121206bd4fef9d800ec5f339598c
Reinaldo Chaves <reichaves@gmail.com>
2022-01-11 22:00:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
+from dash import html
 import requests
 import pandas as pd
-import dash_core_components as dcc
+from dash import dcc
 import plotly.express as px
 import numpy as np
 from dash.dependencies import Input,Output
-import dash_table
+from dash import dash_table
 
 app = dash.Dash(external_stylesheets = [ dbc.themes.FLATLY],)
 
